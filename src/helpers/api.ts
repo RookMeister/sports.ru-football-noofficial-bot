170766c44e4acece49334c90f037d67beacd4966
RookMeister <rookmeister666@gmail.com>
2022-01-06 12:01:24 +1000

    add api

---
+++ src/helpers/api.ts
@@ -0,0 +1,81 @@
+import {
+  ISportsTeaserResponse,
+  ISportsTournamentsListResponse,
+  ISportsTournamentTableResponse,
+  ISportsTournamentMatchesResponse,
+  ISportsGoalsResponse,
+  ISportsMatchResponse,
+  ISportsTournamentPlayersStatResponse,
+  IDataMatches
+} from '@bot/interfaces/sports.ru.interface';
+
+const _importDynamic = new Function('modulePath', 'return import(modulePath)')
+
+async function fetch(...args) {
+  const {default: fetch} = await _importDynamic('node-fetch');
+  return fetch(...args);
+}
+
+export default async function request<T>(url: string) : Promise<T> {
+  try {
+    const response = await fetch(url);
+    const data =  await response.json() as Promise<T>;
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// URL Statistics
+export async function getMatches(ids: string[]): Promise<IDataMatches>  {
+  const requests =
+    ids.map(id => request(`https://www.sports.ru/core/stat/match/online/?args={"id":${id}}`));
+  const response = await Promise.all(requests);
+  const data = [];
+  const tournamentIndex = {};
+  response.forEach((m: ISportsMatchResponse) => {
+    if (typeof tournamentIndex[m.tournament.id] === 'undefined') {
+      tournamentIndex[m.tournament.id] = Object.keys(tournamentIndex).length;
+      data.push({ name: `${m.tournament.name} ${m.tournament.stage_name}`, matches: [] });
+    }
+    // !tournamentIndex[m.tournament.id] && (tournamentIndex[m.tournament.id] = i) && (data.push({ name: m.tournament.id, matches: [] }));
+    data[tournamentIndex[m.tournament.id]].matches.push(m);
+    // matches[i].matches.push({
+    //   status: { name: m.status_name, id: m.status_id },
+    //   start_time: m.start_time,
+    //   online_url: m.page_info.desktop_url,
+    //   first_team: m.first_team,
+    //   second_team: m.second_team
+    // });
+  })
+
+  return data;
+}
+export async function getTeaserMatches(): Promise<ISportsTeaserResponse>  {
+  const url = 'https://www.sports.ru/core/stat/match/teaser/';
+  return await request(url);
+}
+export async function getGoalsMatch(id: number): Promise<ISportsGoalsResponse>  {
+  const url = `https://www.sports.ru/core/stat/match/goals/?args={"id":${id}}`;
+  return await request(url);
+}
+export async function getTornaments(): Promise<ISportsTournamentsListResponse>  {
+  const url = `https://www.sports.ru/core/stat/gadget/tournament_list/?args={"sport_id":208}`;
+  return await request(url);
+}
+export async function getTornamentTable(tournament_id: string): Promise<ISportsTournamentTableResponse>  {
+  const url = `https://www.sports.ru/core/stat/gadget/tournament_table/?args={"tournament_id":${tournament_id}}`;
+  return await request(url);
+}
+export async function getTornamentLastMatches(tournament_id: string): Promise<ISportsTournamentMatchesResponse>  {
+  const url = `https://www.sports.ru/core/stat/gadget/last_matches/?args={"tournament_id":${tournament_id}}`;
+  return await request(url);
+}
+export async function getTornamentFutureMatches(tournament_id: string): Promise<ISportsTournamentMatchesResponse>  {
+  const url = `https://www.sports.ru/core/stat/gadget/future_matches/?args={"tournament_id":${tournament_id}}`;
+  return await request(url);
+}
+export async function getTornamentPlayersStat(tournament_id: string): Promise<ISportsTournamentPlayersStatResponse>  {
+  const url = `https://www.sports.ru/core/stat/gadget/players_stat/?args={"tournament_id":${tournament_id}}`;
+  return await request(url);
+}
