ab1b49bae1625e834c9cff4d582e423b1f70fbab
RookMeister <rookmeister666@gmail.com>
2022-01-07 07:58:34 +1000

    fix

--- src/handlers/matches.handler.ts
+++ src/handlers/matches.handler.ts
@@ -13,7 +13,7 @@ export const matchesHandler = async (ctx: Context, update = false) => {
   // const matches = await getTeaserMatches();
   // const info = convertTeaserData(matches);
   const ids = await MatchesModel.getTodayMatches();
-  const matches = await getMatches(ids);
+  const matches = ids ? await getMatches(ids) : ;
   const info = convertTeaserData(matches, ctx.dbuser.timeZone);
   if (update) {
     await ctx.editMessageText(info, { disable_web_page_preview: true, parse_mode: 'HTML', ...keyboard }).catch((err) => ctx.answerCbQuery('Уже выведено'));
@@ -22,34 +22,24 @@ export const matchesHandler = async (ctx: Context, update = false) => {
   }
 };
 
-function convertTeaserData(matches: IDataMatches, timeZone: string) {
-  const reducer = (previousValue, currentValue) => previousValue + currentValue;
-  // const matches = data.teaser.tournaments;
-  const res = [];
-  // matches.forEach(t => {
-  for (const t of matches) {
-    // if (t.sport.id === 208) {
-      res.push(`\r\n<b><i>${t.name}</i></b>\r\n`);
-      for (const m of t.matches) {
-        const date = setTime(timeZone, m.start_time.full).split(', ');
-        let string = `<b>${date[0]}</b> <a href="${m.page_info.desktop_url}">${m.first_team.name} - ${m.second_team.name}</a>`;
-        string += ` ${m.status_id > 1 ? m.score + ' ' + m.status_name : `в ${date[1]}`}\r\n`;
-        // if (m.first_team.score || m.second_team.score) {
-        //   string += ` (<a href="https://www.sports.ru/stat/match/goals/${m.id}.html">Голы</a>)\r\n`;
-        // } else {
-        //   string += '\r\n';
-        // }
-        res.push(string);
-        // res.push(`<b>${m.start_time.short_date}</b> <a href="https://www.sports.ru${m.online_url}">${m.first_team.name} - ${m.second_team.name}</a> ${m.status.id > 1 ? m.score + ' ' + m.status.name : m.status.name} (<a href="https://www.sports.ru/stat/match/goals/${m.id}.html">Голы</a>)\r\n`);
-        // const { first_team, second_team } = await getGoalsMatch(m.id);
-        // const goals = [...first_team.goals, ...second_team.goals].sort((a,b) => a.minute - b.minute);
-        // goals.forEach(g =>  res.push(`${g.minute} ${g.name} ${g.action ? `(${g.action})` : g.assists[0] ? `(${g.assists[0]})` : ''}\r\n`));
-      }
-      // t.matches.forEach(m => {
-      //   res.push(`<b>${m.start_time.short_date}</b> <a href="https://www.sports.ru${m.online_url}">${m.first_team.name} - ${m.second_team.name}</a> ${m.progress ? m.score + ' ' + m.status.name : m.status.name}\r\n`);
-      // })
-    // }
-  };
-  const string = res.reduce(reducer);
-  return string;
+function convertTeaserData(matches: IDataMatches | null, timeZone: string) {
+  if (matches) {
+    const reducer = (previousValue, currentValue) => previousValue + currentValue;
+    // const matches = data.teaser.tournaments;
+    const res = [];
+    // matches.forEach(t => {
+    for (const t of matches) {
+      // if (t.sport.id === 208) {
+        res.push(`\r\n<b><i>${t.name}</i></b>\r\n`);
+        for (const m of t.matches) {
+          const date = setTime(timeZone, m.start_time.full).split(', ');
+          let string = `<b>${date[0]}</b> <a href="${m.page_info.desktop_url}">${m.first_team.name} \u2014 ${m.second_team.name}</a>`;
+          string += ` ${m.status_id > 1 ? m.score + ' ' + m.status_name : `в ${date[1]}`}\r\n`;
+          res.push(string);
+        }
+    };
+    const string = res.reduce(reducer);
+    return string;
+  }
+  return 'Нет подходящих матчей'
 }
--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -27,29 +27,32 @@ export default async function request<T>(url: string) : Promise<T> {
 }
 
 // URL Statistics
-export async function getMatches(ids: string[]): Promise<IDataMatches>  {
-  const requests =
-    ids.map(id => request(`${process.env.API_SPORTSRU_ONLINE}?args={"id":${id}}`));
-  const response = await Promise.all(requests);
-  const data = [];
-  const tournamentIndex = {};
-  response.forEach((m: ISportsMatchResponse) => {
-    if (typeof tournamentIndex[m.tournament.id] === 'undefined') {
-      tournamentIndex[m.tournament.id] = Object.keys(tournamentIndex).length;
-      data.push({ name: `${m.tournament.name} ${m.tournament.stage_name}`, matches: [] });
-    }
-    // !tournamentIndex[m.tournament.id] && (tournamentIndex[m.tournament.id] = i) && (data.push({ name: m.tournament.id, matches: [] }));
-    data[tournamentIndex[m.tournament.id]].matches.push(m);
-    // matches[i].matches.push({
-    //   status: { name: m.status_name, id: m.status_id },
-    //   start_time: m.start_time,
-    //   online_url: m.page_info.desktop_url,
-    //   first_team: m.first_team,
-    //   second_team: m.second_team
-    // });
-  })
+export async function getMatches(ids: string[] | null): Promise<IDataMatches | null>  {
+  if (ids) {
+    const requests =
+      ids.map(id => request(`${process.env.API_SPORTSRU_ONLINE}?args={"id":${id}}`));
+    const response = await Promise.all(requests);
+    const data = [];
+    const tournamentIndex = {};
+    response.forEach((m: ISportsMatchResponse) => {
+      if (typeof tournamentIndex[m.tournament.id] === 'undefined') {
+        tournamentIndex[m.tournament.id] = Object.keys(tournamentIndex).length;
+        data.push({ name: `${m.tournament.name} ${m.tournament.stage_name}`, matches: [] });
+      }
+      // !tournamentIndex[m.tournament.id] && (tournamentIndex[m.tournament.id] = i) && (data.push({ name: m.tournament.id, matches: [] }));
+      data[tournamentIndex[m.tournament.id]].matches.push(m);
+      // matches[i].matches.push({
+      //   status: { name: m.status_name, id: m.status_id },
+      //   start_time: m.start_time,
+      //   online_url: m.page_info.desktop_url,
+      //   first_team: m.first_team,
+      //   second_team: m.second_team
+      // });
+    })
 
-  return data;
+    return data;
+  }
+  return null;
 }
 export async function getTeaserMatches(): Promise<ISportsTeaserResponse>  {
   const url = process.env.API_SPORTSRU_TEASER;
