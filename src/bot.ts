474e9a31c273cebb2dc71e125fd286e3e651479d
RookMeister <rookmeister666@gmail.com>
2022-01-06 12:02:02 +1000

    change main bot

--- src/bot.ts
+++ src/bot.ts
@@ -1,19 +1,17 @@
 import { Telegraf } from 'telegraf';
 import config from '@bot/config';
+import { initAgenda } from '@bot/helpers/agenda';
 
-import { selectData } from '@bot/helpers/callback-data';
+// import { selectData } from '@bot/helpers/callback-data';
 
 import { setupLoggerMiddleware } from '@bot/middlewares/setup-logger.middleware';
 import { setupSessionMiddleware } from '@bot/middlewares/setup-session.middleware';
-import { setupLocalizationsMiddleware } from '@bot/middlewares/setup-localizations.middleware';
 import { debugLoggerMiddleware } from '@bot/middlewares/debug-logger.middleware';
 import { attachUserMiddleware } from '@bot/middlewares/attach-user.middleware';
 import { setLastActionToUserMiddleware } from '@bot/middlewares/last-action-to-user.middleware';
 
 import { startCommandHandler } from '@bot/handlers/start-command.handler';
-import { languageMenuSelectHandler, languageCommandHandler } from '@bot/handlers/language-selector.handler';
-
-const { match } = require('telegraf-i18n');
+import { matchesHandler } from '@bot/handlers/matches.handler';
 
 const bot = new Telegraf(config.BOT_TOKEN, {
   telegram: {
@@ -21,18 +19,19 @@ const bot = new Telegraf(config.BOT_TOKEN, {
   },
 });
 
+initAgenda();
+
 bot.use(setupLoggerMiddleware());
 bot.use(setupSessionMiddleware());
-bot.use(setupLocalizationsMiddleware());
 bot.use(debugLoggerMiddleware());
 bot.use(attachUserMiddleware);
 bot.use(setLastActionToUserMiddleware);
 
 bot.start(startCommandHandler);
 
-bot.command('language', languageCommandHandler);
-bot.action(selectData('select-language').filter(), languageMenuSelectHandler);
+bot.hears('Матчи', (ctx) => matchesHandler(ctx));
+bot.action('update-matches', (ctx) => matchesHandler(ctx, true));
 
-bot.on('text', (ctx) => ctx.reply(ctx.i18n.t('messageError')));
+bot.on('text', (ctx) => ctx.reply('Извини, я не могу тебя понять. Используй кнопки. Если не видишь кнопки, отправь мне /start'));
 
 export default bot;
