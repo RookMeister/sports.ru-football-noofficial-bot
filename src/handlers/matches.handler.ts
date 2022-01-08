f0032f198d54f256ec422144b551bada7c47a344
RookMeister <rookmeister666@gmail.com>
2022-01-09 07:27:11 +1000

    fix regex

--- src/handlers/matches.handler.ts
+++ src/handlers/matches.handler.ts
@@ -39,6 +39,7 @@ async function convertTeaserData(matches: IDataMatches | null, timeZone: string)
       string += `<a href="${m.page_info.desktop_url}">${m.first_team.name} \u2014 ${m.second_team.name}</a> `;
       if (m.status_id > 1) {
         string += `${m.score + ' ' + m.state_name}`;
+        const regex = `/${m.first_team.name} | ${m.second_team.name}/gm`;
         const review = await ReviewsModel.findReview(`${m.first_team.name} \u2014 ${m.second_team.name}`);
         if (review) {
           string += ` <a href="${review.url}">Обзор матча</a>\r\n`
