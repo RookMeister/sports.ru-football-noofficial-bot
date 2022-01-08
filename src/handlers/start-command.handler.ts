40562028f83d351c4cde11b1ec9597dbf372d5f4
RookMeister <rookmeister666@gmail.com>
2022-01-08 22:01:58 +1000

    change start text

--- src/handlers/start-command.handler.ts
+++ src/handlers/start-command.handler.ts
@@ -6,5 +6,5 @@ export const startCommandHandler = async (ctx: Context) => {
   const { username } = ctx.dbuser;
   const { size, column, values } = mainMenu;
   const keyboard = replyKeyboard(values, size, column)
-  await ctx.reply(`Привет @${username}.\nЭто неофициальный бот Sports.ru`, { ...keyboard });
+  await ctx.reply(`Привет @${username}.\nЭто неофициальный бот Sports.ru\nЗдесь можно узнать расписание матчей (время будет в соответствии тем часовым поясом, который вы установите по команде /time). Посмотреть обзоры после завершения матчей (когда они появятся на YouTube у правообладателей). Следить за статистикой чемпионатов. В скором времени можно будет читать новости с сайта Sports.Ru\nПо любым вопросам и предложениям обращаться к @Rookmeister`, { ...keyboard });
 };
