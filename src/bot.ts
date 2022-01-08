7ded15e55d2529b449cb312371753360cffb9df4
RookMeister <rookmeister666@gmail.com>
2022-01-08 20:23:33 +1000

    add all in bot

--- src/bot.ts
+++ src/bot.ts
@@ -8,12 +8,16 @@ import { setupLoggerMiddleware } from '@bot/middlewares/setup-logger.middleware'
 import { setupSessionMiddleware } from '@bot/middlewares/setup-session.middleware';
 import { debugLoggerMiddleware } from '@bot/middlewares/debug-logger.middleware';
 import { attachUserMiddleware } from '@bot/middlewares/attach-user.middleware';
+import { analyticsMiddleware } from '@bot/middlewares/setup-analytics.middleware';
 import { setLastActionToUserMiddleware } from '@bot/middlewares/last-action-to-user.middleware';
 
 import { startCommandHandler } from '@bot/handlers/start-command.handler';
 import { matchesHandler } from '@bot/handlers/matches.handler';
+import { statCommandHandler, statTournamentTabletHandler } from '@bot/handlers/statistics.handler';
 import { timeCommandHandler, setTimeZoneHandler } from '@bot/handlers/timezone.handler';
 import { sendChangelogCommandHandler } from '@bot/handlers/send-changelog-command.handler';
+import { changelogCommandHandler } from '@bot/handlers/changelog.command';
+import { statBotCommandHandler, statBotMenuSelectHandler } from '@bot/handlers/stat-bot.command';
 
 const bot = new Telegraf(config.BOT_TOKEN, {
   telegram: {
@@ -26,6 +30,7 @@ initAgenda();
 bot.use(setupLoggerMiddleware());
 bot.use(setupSessionMiddleware());
 bot.use(debugLoggerMiddleware());
+bot.use(analyticsMiddleware);
 bot.use(attachUserMiddleware);
 bot.use(setLastActionToUserMiddleware);
 
@@ -34,10 +39,17 @@ bot.start(startCommandHandler);
 bot.hears('Матчи', (ctx) => matchesHandler(ctx));
 bot.action('update-matches', (ctx) => matchesHandler(ctx, true));
 
+bot.hears('Статистика', (ctx) => statCommandHandler(ctx));
+bot.action(selectData('select-tournament').filter(), (ctx) => statTournamentTabletHandler(ctx));
+
 bot.command('time', timeCommandHandler);
 bot.action(selectData('select-time').filter(), setTimeZoneHandler);
 
 bot.command('sendChangeLog', (ctx) => sendChangelogCommandHandler(ctx));
+bot.command('changelog', (ctx) => changelogCommandHandler(ctx));
+
+bot.command('stat', (ctx) => statBotCommandHandler(ctx, 'now', false));
+bot.action(selectData('select-stat-bot').filter(), statBotMenuSelectHandler);
 
 bot.on('text', (ctx) => ctx.reply('Извини, я не могу тебя понять. Используй кнопки. Если не видишь кнопки, отправь мне /start'));
 
