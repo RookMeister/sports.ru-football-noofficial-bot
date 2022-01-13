98ea801efd759d2f4322c930afed7767a4f72bb3
RookMeister <rookmeister666@gmail.com>
2022-01-13 14:38:55 +1000

    fix add unique sports_id

--- src/models/tournamnets.model.ts
+++ src/models/tournamnets.model.ts
@@ -4,7 +4,7 @@ import { TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
 @modelOptions({ schemaOptions: { timestamps: true } })
 export class Tournaments extends TimeStamps {
   @prop({ required: true, default: '' }) name: string;
-  @prop({ required: true, default: 0 }) sports_id: number;
+  @prop({ required: true, default: 0, unique: true }) sports_id: number;
   @prop({ required: true, default: false }) is_top: boolean;
 }
 
