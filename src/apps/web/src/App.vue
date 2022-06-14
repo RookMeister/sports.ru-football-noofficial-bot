cc37fec2b05e8a470f5b1821a53e10d6555cd25e
RookMeister <rookmeister666@gmail.com>
2022-06-14 17:08:28 +1000

    change

--- src/apps/web/src/App.vue
+++ src/apps/web/src/App.vue
@@ -1,5 +1,22 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import ApiService from './services/ApiService';
+
+
+onMounted( async () => {
+  try {
+    console.log(233);
+    await ApiService.apiGetAllTournaments()
+    // console.log('users', users.value)
+  } catch (err) {
+    console.log('err', err)
+  }
+})
+</script>
+
+
 <template>
   <div class="p-8">
-    888
+    888999
   </div>
 </template>
