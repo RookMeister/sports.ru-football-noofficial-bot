c8bc79327dc814c222ba6d38e3a4df91e6fd2d33
RookMeister <rookmeister666@gmail.com>
2022-06-15 14:58:12 +1000

    fix for gh page

--- src/apps/web/src/main.ts
+++ src/apps/web/src/main.ts
@@ -2,22 +2,44 @@ import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
-import Home from './pages/Home.vue';
-import Tournaments from './pages/Tournaments.vue';
-import TournamentsId from './pages/TournamentsId.vue';
+// import Home from './pages/Home.vue';
+// import Tournaments from './pages/Tournaments.vue';
+// import TournamentsId from './pages/TournamentsId.vue';
 
 // css
 import './assets/css/vendor.css';
 
-const routes = [
-  { path: '/', component: Home },
-  { path: '/tournaments', component: Tournaments },
-  { path: '/tournaments/:id', name: 'tournamentsId', component: TournamentsId },
-]
+// const routes = [
+//   { path: '/', component: Home },
+//   { path: '/tournaments', component: Tournaments },
+//   { path: '/tournaments/:id', name: 'tournamentsId', component: TournamentsId },
+// ]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes: [
+    // Does not allow string templating, also @/ is resolved to ../
+    ...Object.entries(import.meta.glob('../pages/**/*.vue')).map(
+      ([p, component]) => {
+        const PAGE_DIR = '../pages'
+        const PAGE_EXT = '.vue'
+        const PAGE_INDEX = 'index'
+
+        let path = p.substring(PAGE_DIR.length, p.length - PAGE_EXT.length)
+        if (path.endsWith(PAGE_INDEX)) {
+          path = path.substring(0, path.length - PAGE_INDEX.length - 1)
+        }
+        if (!path) {
+          path = '/'
+        }
+
+        return {
+          path,
+          component,
+        }
+      },
+    ),
+  ],
 });
 
 const app = createApp(App);
--- src/apps/web/vite.config.ts
+++ src/apps/web/vite.config.ts
@@ -1,10 +1,53 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { resolve as pathResolve, join as pathJoin } from 'path';
+import { sync as glob } from 'fast-glob';
+import { existsSync, mkdirSync, copyFileSync } from 'fs';
 
-const path = require('path');
+function cloneIndexHtmlPlugin(routes: string[] = []): PluginOption {
+  const name = 'CloneIndexHtmlPlugin'
+  const outDir = 'dist' // config's `build.outDir`
+  const src = pathJoin(outDir, 'index.html')
+
+  return {
+    name,
+    closeBundle: () => {
+      const PAGE_EXT = '.vue'
+      const PAGE_INDEX = 'index'
+
+      routes.push(
+        ...glob(`**/*${PAGE_EXT}`, {
+          cwd: 'src/pages',
+        })
+          .map((p) => {
+            p = p.substring(0, p.length - PAGE_EXT.length)
+            if (p.endsWith('/' + PAGE_INDEX)) {
+              p = p.substring(0, p.length - PAGE_INDEX.length - 1)
+            }
+
+            return p
+          })
+          .filter((p) => p !== PAGE_INDEX),
+      )
+
+      routes.map((p) => {
+        const dir = pathResolve(outDir, p)
+        if (!existsSync(dir)) {
+          mkdirSync(dir, { recursive: true })
+        }
+
+        const dst = pathJoin(outDir, p, 'index.html')
+
+        copyFileSync(src, dst)
+        console.log(`${name}: Copied ${src} to ${dst}`)
+      })
+    },
+  }
+}
 
 export default defineConfig({
-  base: process.env.GH ? '/sports.ru-football-noofficial-bot' : '/',
+  base: '/sports.ru-football-noofficial-bot',
+  // base: process.env.GH ? '/sports.ru-football-noofficial-bot' : '/',
   server: {
     watch: {
       ignored: [
@@ -15,8 +58,8 @@ export default defineConfig({
   },
   resolve:{
     alias:{
-      '@' : path.resolve(__dirname, './src')
+      '@' : pathResolve(__dirname, './src')
     },
   },
-  plugins: [ vue() ]
+  plugins: [ vue(), cloneIndexHtmlPlugin() ]
 })
