6b0169ce3434ba14407e845cda1f9082d43fb603
RookMeister <rookmeister666@gmail.com>
2022-06-15 22:32:42 +1000

    add history legth

--- src/apps/web/src/App.vue
+++ src/apps/web/src/App.vue
@@ -1,6 +1,13 @@
+<script setup lang="ts">
+import { getHistoryLength } from '@web/services/historyLength';
+
+const { length } = getHistoryLength()
+</script>
+
 <template>
   <div class="px-6 py-2">
-    <div @click="$router.go(-1)" class="mb-4" :style="{opacity: history.length === 1 ? 0 : 1}">
+    {{ length }}
+    <div @click="$router.go(-1)" class="mb-4" :style="{opacity: length === 1 ? 0 : 1}">
       <svg style="fill: currentColor;" class="w-8 h-8" viewBox="0 0 320 512"><path d="M224 480c-8.188 0-16.38-3.125-22.62-9.375l-192-192c-12.5-12.5-12.5-32.75 0-45.25l192-192c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L77.25 256l169.4 169.4c12.5 12.5 12.5 32.75 0 45.25C240.4 476.9 232.2 480 224 480z"/></svg>
     </div>
     <RouterView />
