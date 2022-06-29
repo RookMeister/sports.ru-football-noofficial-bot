45a74e2edcad32709b3516878faa37d2b373f4ff
RookMeister <rookmeister666@gmail.com>
2022-06-29 20:00:43 +1000

    change vite config

--- src/apps/web/vite.config.ts
+++ src/apps/web/vite.config.ts
@@ -5,6 +5,13 @@ import { resolve as pathResolve } from 'path';
 export default defineConfig({
   base: '/',
   server: {
+    port: 3000,
+    proxy: {
+      '^/api': {
+        target: 'http://api:4000',
+        changeOrigin: true
+      },
+    },
     watch: {
       ignored: [
         '!**/node_modules/**'
